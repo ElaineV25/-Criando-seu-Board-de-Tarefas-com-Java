331d3cf5af9c6b7dda5f7e21bea2cf1747c41a6a
elaine <elainevieira@hotmail.com>
2025-05-28 15:05:54 -0300

    atualização commit

---
+++ bin/main/resources/db/changelog/migrations/db.changelog-202505271938.sql
@@ -0,0 +1,13 @@
+--liquibase formatted sql
+--changeset junior:202505271938
+--comment: boards table create
+
+CREATE TABLE BOARDS(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+) ENGINE=InnoDB;
+
+--rollback DROP TABLE BOARDS
+
+
+
---
+++ src/main/java/br/com/dio/persistence/migration/ClassLoaderResourceAccessor.java
@@ -0,0 +1,5 @@
+package main.java.br.com.dio.persistence.migration;
+
+public class ClassLoaderResourceAccessor {
+
+}
